6935ca780d605107011b0a2de9b5ed5c414bda40
TheValduga <lukinhas.valduga@gmail.com>
2022-10-03 09:45:11 -0300

    .jsx commit

---
+++ React-calc.jsx
@@ -0,0 +1,18 @@
+/*  Calculadora web com react
+    Lucas Gusmão Valduga    */
+
+
+function Calculadora(props) {
+    return (
+        div
+    );
+}
+
+function Body(props) {
+    return (
+    <div>
+        <h1>Lucas Gusmão Valduga</h1>
+        <Calculadora />
+    </div>
+    );
+}
