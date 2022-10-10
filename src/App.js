6b50e2ee8bea4ca554206ab36bf5964a3c41aa98
TheValduga <lukinhas.valduga@gmail.com>
2022-10-10 14:38:58 -0300

    operations

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useState } from "react";
 import './App.css';
 
 function App() {
-  document.title='ReactCalc';
+  document.title = 'ReactCalc';
 
   return (
     <div className="App">
@@ -17,15 +17,16 @@ function App() {
 }
 
 function Calculator() {
-  const [previous,setPrevious] = useState('')
-  const [current,setCurrent] = useState('')
-  const [operation,setOperation] = useState('')
+  const [previous, setPrevious] = useState('')
+  const [current, setCurrent] = useState('')
+  const [result, setResult] = useState('')
+  const [operation, setOperation] = useState('')
 
   function handleClick(e) {
-    const value= e.target.innerText;
+    const value = e.target.innerText;
     if (+value >= 0 || value === ',') {
       addDigit(value);
-    } else if(value.substr(0,1) === 'C') {
+    } else if (value.substr(0, 1) === 'C') {
       clean(value);
     } else if (value === 'DEL') {
       del();
@@ -35,41 +36,83 @@ function Calculator() {
   }
 
   function del() {
-    setCurrent(current.slice(0,-1));
+    setCurrent(current.slice(0, -1));
   }
 
   function clean(value) {
     setCurrent('');
     if (value.length === 1) {
       setPrevious('');
-      setOperation('');
+      setResult('');
+      setOperation('')
     }
   }
 
   function addDigit(value) {
-    if (current.includes(',') && value ===',') {
+
+    if (current.includes(',') && value === ',') {
       return
     }
-    setCurrent(current+value);
-    
+    if (previous === '') {
+      setCurrent('')
+    }
+    setCurrent(current + value);
+
   }
 
   function processOperation(value) {
+    
 
-  }
+      if (value === '=') {
+        let a = previous+current
+        a = eval(a)
+        console.log(a)
+        setResult(a);
+        console.log(result)
+        setPrevious('')
+        setCurrent(a)
+      } else {
+        setPrevious(current+value)
+        setCurrent(result)
+      }
+      
+    }
+
+    /*if (current === result || current === '') {
+      return;
+    } else {
+      if (value != '=') {
+        if (current === '') {
+          setPrevious(current+value)
+          // setOperation(value);
+          setCurrent('')
+        } else {
+          let x = previous+current
+          setResult(eval(x));
+          setPrevious(previous+current+value);
+          setCurrent(result);
+          // setOperation(value)
+        }
+      } else {
+        setResult(eval(previous+current));
+        setPrevious('')
+        setCurrent(result)
+      }
+    }*/
+  
 
   return (
     <div id='calc'>
       <h3>Calculadora</h3>
-      <Display previous={previous} current={current}/>
-      <Buttons func={handleClick}/>
+      <Display previous={previous} current={current} />
+      <Buttons func={handleClick} />
     </div>
   )
 }
 
 function Display(props) {
-  return (  
-    <div id='display'> 
+  return (
+    <div id='display'>
       <div id='previous'>{props.previous}</div>
       <div id='current'>{props.current}</div>
     </div>
@@ -77,27 +120,27 @@ function Display(props) {
 }
 
 function Buttons(props) {
-  
+
   return (
     <div id='buttons_container'>
       <button className="op" onClick={props.func}>CE</button>
-      <button className="op"  onClick={props.func}>C</button>
-      <button className="op"  onClick={props.func}>DEL</button>
-      <button className="op"  onClick={props.func}>/</button>
-      <button className="number"  onClick={props.func}>7</button>
-      <button className="number"  onClick={props.func}>8</button>
-      <button className="number"  onClick={props.func}>9</button>
-      <button className="op"  onClick={props.func}>*</button>
-      <button className="number"  onClick={props.func}>4</button>
-      <button className="number"  onClick={props.func}>5</button>
-      <button className="number"  onClick={props.func}>6</button>
-      <button className="op"  onClick={props.func}>-</button>
-      <button className="number"  onClick={props.func}>1</button>
-      <button className="number"  onClick={props.func}>2</button>
-      <button className="number"  onClick={props.func}>3</button>
-      <button className="op"  onClick={props.func}>+</button>
-      <button className="number"  onClick={props.func}>0</button>
-      <button className="number"  onClick={props.func}>,</button>
+      <button className="op" onClick={props.func}>C</button>
+      <button className="op" onClick={props.func}>DEL</button>
+      <button className="op" onClick={props.func}>/</button>
+      <button className="number" onClick={props.func}>7</button>
+      <button className="number" onClick={props.func}>8</button>
+      <button className="number" onClick={props.func}>9</button>
+      <button className="op" onClick={props.func}>*</button>
+      <button className="number" onClick={props.func}>4</button>
+      <button className="number" onClick={props.func}>5</button>
+      <button className="number" onClick={props.func}>6</button>
+      <button className="op" onClick={props.func}>-</button>
+      <button className="number" onClick={props.func}>1</button>
+      <button className="number" onClick={props.func}>2</button>
+      <button className="number" onClick={props.func}>3</button>
+      <button className="op" onClick={props.func}>+</button>
+      <button className="number" onClick={props.func}>0</button>
+      <button className="number" onClick={props.func}>,</button>
       <button id="equal" onClick={props.func}>=</button>
     </div>
   )
